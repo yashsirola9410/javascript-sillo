83b22cf189a3b23a965af0b172aae01f3fd060f9
yashsirola9410 <yashtechid9410@gmail.com>
2024-10-05 01:35:32 +0530

    commmit 5

---
+++ basics/l7data-conversion.js
@@ -0,0 +1,69 @@
+let score = " yash"
+console.log(score)
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valuetono = Number(score)     // isse  kuch bhi number mai convert ho jata hai 
+console.log(typeof valuetono);
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let islogged = "yash"
+
+let booleanconversion = Boolean(islogged);  //kuch bhi  boolean mai convert ho jayega
+console.log(islogged);
+console.log(booleanconversion);
+
+// 1 => true; 0 => false
+// "" => false
+// "yash" => true
+
+
+// NaN - not a number 
+
+let someno = 33
+let tostring = String(someno);
+
+console.log(tostring)
+console.log(typeof string)
+
+
+
+// ****operations******
+
+let value = 6 
+let negvalue = -value
+console.log(negvalue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "world"
+let str3 = str1+str2
+console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+ //https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
