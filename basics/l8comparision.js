4699de3df5699ed81fc8ceb2fcdde2bfef8faed5
yashsirola9410 <yashtechid9410@gmail.com>
2024-10-06 20:09:09 +0530

    commit l8comparision

---
+++ basics/l8comparision.js
@@ -0,0 +1,26 @@
+//Ye hai normal wale comparision 
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+//YE hai string  wale comparision 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+//Ye hai complicated wale comparision 
+// console.log(null > 0);  //ye false return karega kyuki comparison operator such as > <= jo sign hote hai vo null ko no mai convert kar dete hai
+// console.log(null == 0);  //  true
+// console.log(null >= 0);    // true 
+
+console.log(undefined == 0);     // false 
+console.log(undefined > 0);      //false
+console.log(undefined < 0);       // false
+
+// === 
+
+console.log("2" === 2);      // false kyuki === data type ko bhi compare karta hai 
