36e232fea9a4cf57e1f3b4ff14b5737af0fad3f5
yashsirola9410 <yashtechid9410@gmail.com>
2024-12-23 20:00:57 +0530

    changes in the api

---
+++ adv/promise.js
@@ -0,0 +1,13 @@
+
+// PROMISES : - aisa kuch jo baad mai jaa ke complete hota hai instantly nhi hota.
+
+// Promise is a JavaScript object for asynchronous operation.
+
+// ASYNCHRONOUS TASK ::: --  in JavaScript are operations that allow the program to continue executing other
+//   code without waiting for the completion of the task. These tasks are particularly useful when dealing with time-consuming operations like:
+
+// Fetching data from an API.
+// Reading or writing files .
+// Timers or delays.
+// Interacting with databases.
+
